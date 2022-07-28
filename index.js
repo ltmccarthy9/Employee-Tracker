49c48a05263e0bd34686b4ff37283e2c16e95c12
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-28 18:17:14 -0500

    schema

---
+++ index.js
@@ -0,0 +1,28 @@
+// require packages
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const viewAD = "View all departments";
+const addD = "Add a department";
+const addR = "Add a role";
+const addE = "Add an employee";
+const updateRole = "Update an employee role";
+//Connect to our mySQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: ''
+    },
+    console.log('Connected to the database.')
+);
+
+inquirer.prompt([
+    {
+        type: 'list',
+        choices: [viewAD, addD, addR, addE, updateRole],
+        name: options
+    }
+])
+
---
+++ sql/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
+
+DROP TABLE IF EXISTS job_role;
+CREATE TABLE job_role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES job_role(id)
+    ON DELETE SET NULL
+)
