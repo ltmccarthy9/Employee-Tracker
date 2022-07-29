b2eb3e9de71e3087422561e3acc3c2a3198caf4a
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-28 19:52:20 -0500

    show all departments

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 // require packages
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
-const viewAD = "View all departments";
-const addD = "Add a department";
-const addR = "Add a role";
-const addE = "Add an employee";
-const updateRole = "Update an employee role";
+const cTable = require('console.table');
 //Connect to our mySQL database
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: ''
+        database: 'company_db'
     },
     console.log('Connected to the database.')
 );
+
+
+const viewAD = "View all departments";
+const addD = "Add a department";
+const addR = "Add a role";
+const addE = "Add an employee";
+const updateRole = "Update an employee role";
+
 
 inquirer.prompt([
     {
@@ -25,7 +28,10 @@ inquirer.prompt([
         name: 'options'
     }
 ]).then((response) => {
-    console.log("You chose " + response.options + "!");
+    db.query("SELECT * FROM department", function (err, result) {
+        if (err) throw err;
+        console.table(result);
+      });
     
 });
 
