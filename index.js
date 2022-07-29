d8417855e2a396f215d8a10bdd9b129a0169a4e1
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-28 20:10:06 -0500

    inquirere and querties for all show alls

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const { urlToHttpOptions } = require('url');
 //Connect to our mySQL database
 const db = mysql.createConnection(
     {
@@ -15,24 +16,65 @@ const db = mysql.createConnection(
 
 
 const viewAD = "View all departments";
+const viewR = "View all roles";
+const viewE = "View all employees"
 const addD = "Add a department";
 const addR = "Add a role";
 const addE = "Add an employee";
 const updateRole = "Update an employee role";
 
+//Function for continuing or exiting program
+function yesORno(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Continue or Exit?',
+            choices: ["Continue", "Exit"],
+            name: 'continue'
+        }
+    ]).then((response) => {
+        if(response.continue === "Continue"){
+            keepGoing();
+        }else {
+            console.log("goodbye");
+            return null;
+        }
+    })
+}
 
+// Main function
+function keepGoing() {
 inquirer.prompt([
     {
         type: 'list',
-        choices: [viewAD, addD, addR, addE, updateRole],
+        message: 'Choose an option:',
+        choices: [viewAD, viewR, viewE, addD, addR, addE, updateRole],
         name: 'options'
-    }
+    },
 ]).then((response) => {
-    db.query("SELECT * FROM department", function (err, result) {
-        if (err) throw err;
+    if(response.options === viewAD){
+        db.query("SELECT * FROM department", function (err, result) {
+            if (err) throw err;
         console.table(result);
+        yesORno();
       });
-    
+    }else if (response.options === viewR){
+        db.query("SELECT * FROM job_role", function (err, result) {
+            if (err) throw err;
+        console.table(result);
+        yesORno();
+      });
+    }else if (response.options === viewE) {
+        db.query("SELECT * FROM employee", function (err, result) {
+            if (err) throw err;
+        console.table(result);
+        yesORno();
+      });
+    }else if (response.options === addD) {
+        
+    }
 });
+}
 
+keepGoing();
 console.log("all done");
